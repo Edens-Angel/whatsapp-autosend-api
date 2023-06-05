6614185590662b802c4004bd8dca806ef1def8f4
Paul Al-Dabagh <paul.al-dabagh@hva.nl>
2023-06-06 01:17:56 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+require('dotenv').config();
+
+const testMessage = process.env.message
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_NUMBER
+const myNumber = process.env.MY_NUMBER
+const port = process.env.PORT || 3000
+
+const client = require("twilio")(accountSid, authToken)
+const app = express();
+
+app.listen(port, () => {
+ console.log(`Server running on port ${port}`);
+});
+
+const sendMessage = (res, to, message) => {
+    client.messages.create({
+        to: `whatsapp:${to}`,
+        from: `whatsapp:${twilioNumber}`,
+        body: message
+    }).then(({sid}) => res.json(sid))
+    .catch(res.json)
+}
+
+app.get("/env-test", (_, res) => {
+    const message = testMessage || "Env variable 'message' has not been set"
+    console.log(message)
+    
+    res.json(message)
+})
+
+app.post("/send-me", (_, res) => {
+    console.log("message send to me")
+    const message = "this is a automated message"
+
+    sendMessage(res, myNumber, message)
+})
